dbe310de1c0b7b7141cfb89320e41adf5fb1b74b
vulture <189206369+CarrionAnimus@users.noreply.github.com>
2025-04-24 21:09:40 -0500

    feat(boxy): debian toolbox

--- dot_config/zsh/scripts/executable_distrobox.sh
+++ dot_config/zsh/scripts/executable_distrobox.sh
@@ -13,32 +13,54 @@ get_running_containers (){
 # Distrobox remove
 remove_container (){
 	echo "Warning!!! This will automatically stop and remove the selected container."
-	local SELECT_CONTAINER=$(get_containers | fzf)
+	local SELECT_CONTAINER
+    SELECT_CONTAINER=$(get_containers | fzf)
 	echo "${SELECT_CONTAINER} is being removed."
 	echo -e "y" | distrobox-stop "${SELECT_CONTAINER}"
 	echo -e "y" | distrobox-rm "${SELECT_CONTAINER}"
 }
 
 stop_container (){
-	local SELECT_CONTAINER=$(get_running_containers | fzf)
+	local SELECT_CONTAINER
+    SELECT_CONTAINER=$(get_running_containers | fzf)
 	echo "${SELECT_CONTAINER} is being stoped."
 	echo -e "y" | distrobox-stop "${SELECT_CONTAINER}"
 }
 
 create_container (){
-    echo "Create Arch Container"
+    echo "Select Container Variant"
+
+    local CONTAINER_VARIANT=(
+        "ghcr.io/carrionanimus/cachyos-toolbox:latest"
+        "quay.io/toolbx-images/debian-toolbox:unstable"
+    )
+
+    local SELECT_CONTAINER_VARIANT
+    SELECT_CONTAINER_VARIANT=$(printf "%s\n" "${CONTAINER_VARIANT[@]}" | fzf)
+
+    if [ "$SELECT_CONTAINER_VARIANT" = "ghcr.io/carrionanimus/cachyos-toolbox:latest" ]; then
+        local CONTAINER_PACKAGE_CACHE="$HOME/Documents/Distrobox/Cache/Arch:/var/cache/pacman/pkg"
+    elif [ "$SELECT_CONTAINER_VARIANT" = "quay.io/toolbx-images/debian-toolbox:unstable" ]; then
+        local CONTAINER_PACKAGE_CACHE="$HOME/Documents/Distrobox/Cache/Debian:/var/cache/apt/archives"
+    fi
+
+    mkdir -p "$HOME/Documents/Distrobox/Cache/Arch"
+    mkdir -p "$HOME/Documents/Distrobox/Cache/Debian"
+
     local CONTAINER_NAME
-    read -p "Enter Container Name: " CONTAINER_NAME
-    distrobox-create --name $CONTAINER_NAME \
-        --home $HOME/Documents/Distrobox/$CONTAINER_NAME --image ghcr.io/carrionanimus/cachyos-toolbox:latest \
-        --volume $HOME/Documents/Distrobox/Cache/Arch:/var/cache/pacman/pkg \
+    read -rp "Enter Container Name: " CONTAINER_NAME
+
+    distrobox-create --name "$CONTAINER_NAME" \
+        --home "$HOME/Documents/Distrobox/$CONTAINER_NAME" --image "$SELECT_CONTAINER_VARIANT" \
+        --volume "$CONTAINER_PACKAGE_CACHE" \
         --volume /usr/share/vulkan/icd.d/nvidia_icd.x86_64.json:/usr/share/vulkan/icd.d/nvidia_icd.x86_64.json:ro \
         --nvidia
 }
 
 # List and enter selected container
 enter_container (){
-    local SELECT_CONTAINER=$(get_containers | fzf)
+    local SELECT_CONTAINER
+    SELECT_CONTAINER=$(get_containers | fzf)
     distrobox-enter "${SELECT_CONTAINER}"
 }
 
