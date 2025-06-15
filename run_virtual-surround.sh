57d17579405c7108327ec332a156cba67cd5a978
CarrionAnimus <189206369+CarrionAnimus@users.noreply.github.com>
2025-06-14 19:28:39 -0500

    fix: wrong chezmoi command

--- run_virtual-surround.sh
+++ run_virtual-surround.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash env
+#!/usr/bin/env sh
 
 sed -i "s=USERNAME=$USER=g" "$HOME"/.config/pipewire/pipewire.conf.d/virtual-surround-5_1.conf
 sed -i "s=USERNAME=$USER=g" "$HOME"/.config/pipewire/pipewire.conf.d/virtual-surround-7_1.conf
