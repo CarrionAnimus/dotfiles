09a62d83a2baffbcc16f83e61980864321b067f8
CarrionAnimus <189206369+CarrionAnimus@users.noreply.github.com>
2025-06-14 19:08:00 -0500

    fix: surround sound paths

---
+++ .chezmoiscripts/run_virtual-surround.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash env
+
+sed -i "s=USERNAME=$USER=g" "$HOME"/.config/pipewire/pipewire.conf.d/virtual-surround-5_1.conf
+sed -i "s=USERNAME=$USER=g" "$HOME"/.config/pipewire/pipewire.conf.d/virtual-surround-7_1.conf
