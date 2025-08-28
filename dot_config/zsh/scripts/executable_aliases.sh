f8113caa9acfbdc980085e8504a1c6e6b5b64aeb
CarrionAnimus <189206369+CarrionAnimus@users.noreply.github.com>
2025-08-28 11:59:55 -0500

    feat: swww & swaylock

--- dot_config/zsh/scripts/executable_aliases.sh
+++ dot_config/zsh/scripts/executable_aliases.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env sh
 
+# systemctl
 alias reload='systemctl --user daemon-reload'
 alias rwire='systemctl --user restart pipewire pipewire-pulse wireplumber'
+
+# gnome
 alias setup-gnome='~/.config/zsh/scripts/gnome.sh'
 
+# Boxy
 alias boxy='~/.config/zsh/scripts/distrobox.sh'
 alias boxy-stop='~/.config/zsh/scripts/distrobox.sh q'
 alias boxy-rm='~/.config/zsh/scripts/distrobox.sh rm'
+
+# swww
+alias wall-set='~/.config/zsh/scripts/wall-set.sh'
