62c60dff2bad480ebfbb02aef7c0f799d77bfcbc
CarrionAnimus <189206369+CarrionAnimus@users.noreply.github.com>
2026-03-08 13:05:52 -0500

    chore: update containers path
    feat: yazi exit to directory

--- dot_config/zsh/scripts/executable_aliases.sh
+++ dot_config/zsh/scripts/executable_aliases.sh
@@ -11,3 +11,12 @@ alias setup-gnome='~/.config/zsh/scripts/gnome.sh'
 alias boxy='~/.config/zsh/scripts/distrobox.sh'
 alias boxy-stop='~/.config/zsh/scripts/distrobox.sh q'
 alias boxy-rm='~/.config/zsh/scripts/distrobox.sh rm'
+
+# Yazi exit on selected directory https://yazi-rs.github.io/docs/quick-start/#shell-wrapper
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
--- dot_config/zsh/scripts/executable_containers.sh
+++ dot_config/zsh/scripts/executable_containers.sh
@@ -6,8 +6,8 @@ gallery-dl () {
     --interactive \
     --rm \
     --tty \
-    --volume ~/Documents/Containers/Gallery-dl:/etc/gallery-dl:z \
-    --volume ~/Documents/Containers/Gallery-dl/gallery-dl.conf:/etc/gallery-dl.conf:z \
+    --volume ~/.config/containers/storage/Gallery-dl:/etc/gallery-dl:z \
+    --volume ~/.config/containers/storage/Gallery-dl/gallery-dl.conf:/etc/gallery-dl.conf:z \
     ghcr.io/mikf/gallery-dl:latest \
     "$@"
 }
